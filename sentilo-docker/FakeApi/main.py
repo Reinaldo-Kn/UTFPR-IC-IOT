dfe58f88d97d280ce26ca012edc004a48307e00f
Eduardo <eduardoprasniewski@gmail.com>
2023-04-15 10:27:33 -0300

    Fix: data visualization in kibana

--- sentilo-docker/FakeApi/main.py
+++ sentilo-docker/FakeApi/main.py
@@ -1,6 +1,12 @@
 from FakeSensor import *
+from random import randint
+from time import sleep
 
-UTF_PROVIDER_TOKEN = 'a62102c2893cabed976f59162f6d4125059b31ead23a5fa7ce18d9c6627bf426'
+UTF_PROVIDER_TOKEN = 'a7e1972c9ccc71d5c27013a3c60fbe335b1038859f5804630f7e2f3152861370'
 
 fake_temp_sensor = FakeSensor(UTF_PROVIDER_TOKEN, 'Utfpr-TD', 'TEMP_SENSOR')
-fake_temp_sensor.send_data(24)
+while(True):
+    rand = randint(10,35)
+    fake_temp_sensor.send_data(rand)
+    print(rand)
+    sleep(5)
